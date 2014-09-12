80d0971b0976076af25718872601441a5decb8e9
Andrew Mussey <MusseyAM@vt.edu>
2014-09-12 18:58:02 -0400

    Added the delete method to the Database class.

--- database.lua
+++ database.lua
@@ -9,22 +9,22 @@ function Database.init(dbName)
             ["tables"]={}
         })
     end
+    db.name = dbName
     db.db = json.parse(storage[dbName])
     return db
 end
 
 function Database:read(request)
     if request.query["table"] == nil then
-        return 'No table selected.'
+        return {["response"]="error", ["message"]="No table selected."}
     end
-    if self.db["tables"]["" .. request.query["table"]] == nil then
-        --return {}
-    end
-    dbtable = self.db["tables"][request.query["table"]]
+    tableName = "" .. request.query["table"]
     request.query["table"] = nil
-    
-    -- params = getKeys(request.query)
-    
+    if self.db["tables"][tableName] == nil then
+        return {}
+    end
+    dbtable = self.db["tables"][tableName]
+
     dbtable = {
         {
             ["id"]="1",
@@ -66,30 +66,54 @@ function Database:read(request)
             end
         end
     end
-    
-    
-    return dbtable -- params -- self.db
+    return dbtable
 end
 
 function Database:write(request)
     if request.query["table"] == nil then
-        return 'No table selected.'
+        return {["response"]="error", ["message"]="No table selected."}
     end
     return 'write'
 end
 
 function Database:update(request)
     if request.query["table"] == nil then
-        return 'No table selected.'
+        return {["response"]="error", ["message"]="No table selected."}
     end
     return 'update'
 end
 
 function Database:delete(request)
     if request.query["table"] == nil then
-        return 'No table selected.'
+        return {["response"]="error", ["message"]="No table selected."}
     end
-    return 'delete'
+    tableName = "" .. request.query["table"]
+    request.query["table"] = nil
+    if self.db["tables"][tableName] ~= nil then
+        -- Go ahead and delete stuff.
+        dbtable = self.db["tables"][tableName]
+        if #request.query == 0 then
+            dbtable = {}
+        else
+            for value,_ in pairs(request.query) do
+                local i = 1
+                while dbtable[i] ~= nil do
+                    if dbtable[i][value] == nil then
+                        i = i + 1
+                    else
+                        if dbtable[i][value] == request.query[value] then
+                            table.remove(dbtable, i)
+                        else
+                            i = i + 1
+                        end
+                    end
+                end
+            end
+        end
+        self.db["tables"][tableName] = dbtable
+        storage[self.name] = json.stringify(db.db)
+    end
+    return {["response"]="success",["message"]="Delete successful."}
 end
 
 
@@ -100,4 +124,4 @@ end
 local db = Database.init("wooo")
 
 
-return json.stringify(db:read(request))
+return json.stringify(db:delete(request))
